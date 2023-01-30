41ec7026d181d23fd6e5a0b77fc7d927a475e49a
yu.kang <717147581@qq.com>
2023-01-30 21:12:10 +0800

    feat:add program of variable and cin

---
+++ ch2/2.3_getinfo.cpp
@@ -0,0 +1,17 @@
+//
+// Created by kangyu on 23-1-30.
+//
+#include <iostream>
+
+int main(){
+  using namespace std;
+
+  int carrots;
+   cout << "How many carrots do you have?" << endl;
+   cin >> carrots; // C++ input
+   cout << "Here are two more.";
+   carrots = carrots + 2;
+   // the next line concatenates output
+   cout << "Now you have " << carrots << " carrots." << endl;
+   return 0;
+}
