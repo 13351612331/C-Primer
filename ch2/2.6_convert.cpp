1284e3655c4b95724c05aeba270d4fa16100126f
yu.kang <717147581@qq.com>
2023-02-01 22:09:49 +0800

    feat:add an example with return value

---
+++ ch2/2.6_convert.cpp
@@ -0,0 +1,20 @@
+//
+// Created by kangyu on 23-2-1.
+//
+#include <iostream>
+int stonetolb(int); // function prototype
+
+int main(){
+  using namespace std;
+  int stone;
+  cout << "Enter the weight in stone: ";
+  cin >> stone;
+  int pounds = stonetolb(stone);
+  cout << stone << " stone = ";
+  cout << pounds << " pounds." << endl;
+  return 0;
+}
+
+int stonetolb(int sts){
+  return 14 * sts;
+}
