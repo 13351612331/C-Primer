9055f59c49a5ea7bde6885cab55093447b7cbb51
yu.kang <717147581@qq.com>
2023-02-14 22:49:20 +0800

    feat:add 3.8-3.10 examples

---
+++ ch3_处理数据/3.9_fltadd.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kangyu on 23-2-14.
+//
+#include <iostream>
+int main(){
+  using namespace std;
+  float a = 2.34E+22f;
+  float b = a + 1.0f;
+
+  cout << "a = " << a << endl;
+  cout << "b - a = " << b - a << endl;
+  return 0;
+}
